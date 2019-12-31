7f830b742fbf7bb97f58a23ed7ea12793eba5041
Nathan Schaan <nathanschaan@gmail.com>
2019-12-30 20:19:15 -0600

    Added two game logic - need to limit to < 150

--- hand.py
+++ hand.py
@@ -1,17 +1,40 @@
 class Game:
 	def __init__(self):
-		self.usScore = 0
-		self.themScore = 0
+		self.usScore = int(0)
+		self.themScore = int(0)
 
 
-def gameScore(total, handScore):
-	total = total + handScore
-	return(total)
+def gameScore(handScore):
+	if handScore < 0:
+		if gameOne.themScore > 0:
+			print("Second")
+			print("Game1 ",gameOne.themScore)
+			print("Game2 ",gameTwo.themScore)
+			print()
+			gameOne.themScore = (handScore * -1) + gameOne.themScore
+			gameTwo.themScore = (handScore * -1) + gameTwo.themScore
+			return gameTwo.themScore, gameOne.themScore
+		else:
+			print("First")
+			gameOne.themScore = (handScore * -1) + gameOne.themScore
+			return gameOne.themScore
+	else:
+		if gameOne.usScore > 0:
+			gameOne.usScore =+ handScore
+			gameTwo.usScore =+ handScore
+		else:
+			gameOne.usScore =+ handScore
+
 
 gameOne = Game()
+gameTwo = Game()
+
 handScore = -10
-gameOne.themScore = gameScore(gameOne.themScore, handScore)
+gameScore(handScore)
 print(gameOne.themScore)
+print(gameTwo.themScore)
 handScore = -20
-gameOne.themScore = gameScore(gameOne.themScore, handScore)
+gameScore(handScore)
+print()
 print(gameOne.themScore)
+print(gameTwo.themScore)
